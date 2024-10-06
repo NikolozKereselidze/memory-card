b4ed20ff84a0d5808d7c5781ba60ec3d93847a75
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-10-06 19:21:39 +0400

    feat: create Card component
    Fetches data from Pokemon api and updates the state using useEffect hook

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,32 @@
-function Card() {
-  return <h2>Helo</h2>;
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+function Card({ poke, onClick }) {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + poke)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [poke]);
+
+  return (
+    <div className="card">
+      <button onClick={() => onClick(pokemon.name)}>
+        <img
+          className="w-full h-full object-cover"
+          src={pokemon?.sprites.front_shiny}
+          alt="profile-picture"
+        />
+      </button>
+    </div>
+  );
 }
 
 export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Card from "./components/Card";
+import Game from "./components/Game";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Card />
+    <Game />
   </StrictMode>
 );
